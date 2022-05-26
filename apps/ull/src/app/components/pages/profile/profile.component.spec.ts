41e934881e1fc8ae0b1f79f962151cb5430c712a
EnzoFilangi <enzo.filangi@efrei.net>
2022-05-26 22:55:08 +0200

    feat(web) : Header profile page
    - Profile header with cover picture, profile picture, company information and company rating

--- apps/ull/src/app/components/pages/profile/profile.component.spec.ts
+++ apps/ull/src/app/components/pages/profile/profile.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProfileComponent } from './profile.component';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {RouterTestingModule} from "@angular/router/testing";
+import {AccueilComponent} from "../accueil/accueil.component";
 
 describe('ProfileComponent', () => {
   let component: ProfileComponent;
@@ -9,6 +12,13 @@ describe('ProfileComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ProfileComponent],
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule.withRoutes([
+          { path: '', component: AccueilComponent},
+          { path: 'profile', component: AccueilComponent}
+        ])
+      ]
     }).compileComponents();
   });
 
--- apps/ull/src/app/components/pages/profile/profile.component.ts
+++ apps/ull/src/app/components/pages/profile/profile.component.ts
@@ -2,12 +2,16 @@ import {Component, OnInit} from '@angular/core';
 import {UserService} from "../../../services/user-service/user.service";
 import {ProviderProfile} from "@ull/api-interfaces";
 
+import {faStar} from "@fortawesome/free-solid-svg-icons";
+
 @Component({
   selector: 'ull-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
+  faStar = faStar
+
   constructor(
     private userService: UserService
   ) {}
