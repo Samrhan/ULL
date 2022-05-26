3f5a1098e371005499bf1f8eb6d0c126521b501f
EnzoFilangi <enzo.filangi@efrei.net>
2022-05-26 20:15:24 +0200

    feat(web) : Profile interfaces on API, changed the handling of data for the navbar

--- apps/ull/src/app/services/user-service/user.service.ts
+++ apps/ull/src/app/services/user-service/user.service.ts
@@ -1,21 +1,58 @@
 import { Injectable } from '@angular/core';
+import {AuthenticationService} from "../authentication/authentication.service";
+import {HttpClient} from "@angular/common/http";
+
+import {
+  ProviderProfile
+} from "@ull/api-interfaces";
+import {environment} from "../../../environments/environment";
+import {Observable, of, tap} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(
+    private authenticationService: AuthenticationService,
+    private httpClient: HttpClient
+  ) { }
 
-  profilePictureURL() : string {
-    return "../../../../assets/images/cocktails.jpg";
-  }
+  /**
+   * Returns the full profile of the connected user.
+   *
+   * Employs a caching strategy : on the first fetch, the profile is stored to sessionStorage, and this
+   * value is reused for subsequent calls. Use the #force parameter to invalidate the cache.
+   * @param force invalidates the cache and forces a reload from the server
+   */
+  fetchProviderProfile(force : boolean = false) : Observable<ProviderProfile> {
+    return of({
+      id_provider: "5f51f2b9-55d4-4b92-8288-49a3dfbec9cb",
+      company_name: "Catering premium",
+      company_description: "Un service traiteur de qualité",
+      area_served: "Région parisienne",
+      cover_picture: "",
+      profile_picture: "../../../../assets/images/cocktails.jpg",
+      rating: 4.5,
+    } as ProviderProfile)
 
-  companyName() : string {
-    return "Catering premium";
-  }
+    /* Commented out right now to avoid warning from unreachable code
+    if (force) {
+      sessionStorage.removeItem('profile')
+    }
 
-  companyDescription() : string {
-    return "Service traiteur de qualité \nLorem ipsum dolor sit amet, consectetur adipiscing elit";
+    const cachedProfile = sessionStorage.getItem('profile');
+    if (cachedProfile){
+      return of(JSON.parse(cachedProfile));
+    } else {
+      const idProvider = this.authenticationService.getProviderId();
+      return this.httpClient.get<ProviderProfile>(environment.baseServerURL + environment.providerServiceURL + '/provider_profile/' + idProvider)
+        .pipe(
+          tap({
+            next: payload => sessionStorage.setItem('profile', JSON.stringify(payload))
+          })
+        )
+    }
+    */
   }
 }
--- libs/api-interfaces/src/lib/api-interfaces.ts
+++ libs/api-interfaces/src/lib/api-interfaces.ts
@@ -42,3 +42,42 @@ export interface ChangePasswordProviderBody {
   old_password: string,
   new_password: string
 }
+
+export interface Performance {
+  id_performance: string,
+  performance_title: string,
+  performance_description: string
+  price: {
+    value: number,
+    unit: "absolute" | "person" | "stack"
+  },
+  picture: string,
+}
+
+export enum ProviderSectionType {
+  big = "big",
+  medium = "medium",
+  small = "small",
+  info = "info"
+}
+
+export interface ProviderProfileSection {
+  id_section: string,
+  type: ProviderSectionType,
+  section_title: string,
+  section_description: string,
+  content ?: [Performance], // In case of "info" section this property is absent
+  preview_amount ?: number, // For "small" sections
+  pictures ?: [string] // For "big" sections
+}
+
+export interface ProviderProfile {
+  id_provider: string,
+  company_name: string,
+  company_description: string,
+  area_served: string,
+  cover_picture: string,
+  profile_picture: string,
+  rating: number,
+  services: [ProviderProfileSection]
+}
