61a554996007f8cbe5226a61e88a07887003be5a
Jolabuse <jonathan.witt@efrei.net>
2022-06-06 14:10:52 +0200

    rebase feature/mobile/mobile/feature-research onto feature/api/customer/project feat(mobile): research - authentication google and profile pic

--- apps/mobile/lib/main.dart
+++ apps/mobile/lib/main.dart
@@ -7,157 +7,37 @@
 import 'dart:async';
 import 'dart:convert' show json;
 
-import 'package:figma_squircle/figma_squircle.dart';
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-import 'package:http/http.dart' as http;
 import 'package:mobile/screens/authentication.dart';
 import 'package:mobile/screens/mainScreen.dart';
-
-GoogleSignIn _googleSignIn = GoogleSignIn(
-  // Optional clientId
-  clientId:
-      '978419384807-l8725i106c9a3pl60st07i2gs39p1ct2.apps.googleusercontent.com',
-  scopes: <String>[
-    'email',
-    'openid',
-    'https://www.googleapis.com/auth/userinfo.profile',
-  ],
-);
 
 void main() {
   runApp(
     const MaterialApp(
       title: 'Google Sign In',
-      home: SignInDemo(),
+      home: ULL(),
     ),
   );
 }
 
-class SignInDemo extends StatefulWidget {
-  const SignInDemo({Key? key}) : super(key: key);
-
-  @override
-  State createState() => SignInDemoState();
-}
-
-class SignInDemoState extends State<SignInDemo> {
-  GoogleSignInAccount? _currentUser;
-  String _contactText = '';
-
-  @override
-  void initState() {
-    super.initState();
-    _googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount? account) {
-      setState(() {
-        _currentUser = account;
-      });
-      if (_currentUser != null) {
-        _handleGetContact(_currentUser!);
-      }
-    });
-    _googleSignIn.signInSilently();
-  }
-
-  Future<void> _handleGetContact(GoogleSignInAccount user) async {
-    setState(() {
-      _contactText = 'Logging in';
-    });
-    final http.Response response = await http.post(
-      Uri.parse('http://10.0.2.2:3333/api/auth/login'),
-      body: {'email': user.email, 'id': user.id, 'access_token': (await user.authHeaders)['Authorization']},
-    );
-    final Map<String, dynamic> data =
-        json.decode(response.body) as Map<String, dynamic>;
-    setState(() {
-      _contactText = 'Token : $data';
-    });
-  }
-
-  String? _pickFirstNamedContact(Map<String, dynamic> data) {
-    final List<dynamic>? connections = data['connections'] as List<dynamic>?;
-    final Map<String, dynamic>? contact = connections?.firstWhere(
-      (dynamic contact) => contact['names'] != null,
-      orElse: () => null,
-    ) as Map<String, dynamic>?;
-    if (contact != null) {
-      final Map<String, dynamic>? name = contact['names'].firstWhere(
-        (dynamic name) => name['displayName'] != null,
-        orElse: () => null,
-      ) as Map<String, dynamic>?;
-      if (name != null) {
-        return name['displayName'] as String?;
-      }
-    }
-    return null;
-  }
-
-  Future<void> _handleSignIn() async {
-    try {
-      await _googleSignIn.signIn();
-    } catch (error) {
-      print(error);
-    }
-  }
-
-  Future<void> _handleSignOut() => _googleSignIn.disconnect();
-
-  Widget _buildBody() {
-    final GoogleSignInAccount? user = _currentUser;
-    if (user != null) {
-      return Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          ListTile(
-            leading: GoogleUserCircleAvatar(
-              identity: user,
-            ),
-            title: Text(user.displayName ?? ''),
-            subtitle: Text(user.email),
-          ),
-          const Text('Signed in successfully.'),
-          Text(_contactText),
-          ElevatedButton(
-            onPressed: _handleSignOut,
-            child: const Text('SIGN OUT'),
-          ),
-          ElevatedButton(
-            child: const Text('REFRESH'),
-            onPressed: () => _handleGetContact(user),
-          ),
-        ],
-      );
-    } else {
-      return MaterialApp(
-        home :AuthenticationPage()
-        //home : MainScreen()
-      );
-    }
-  }
+class ULL extends StatelessWidget {
+  const ULL({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return MaterialApp(
+      home :Scaffold(
         body: ConstrainedBox(
           constraints: const BoxConstraints.expand(),
-          child: _buildBody(),
-        ));
+          child: AuthenticationPage(),
+        )
+      )
+
+        //home : MainScreen()
+    );
   }
 }
 
-class SkewCut extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    final path = Path();
-    path.lineTo(size.width, 0);
-    path.lineTo(size.width, size.height);
-    path.lineTo(0, 6*size.height/7);
-    path.lineTo(0, 0);
-    path.close();
 
-    return path;
-  }
 
-  @override
-  bool shouldReclip(SkewCut oldClipper) => false;
-}
+
--- apps/mobile/lib/screens/authentication.dart
+++ apps/mobile/lib/screens/authentication.dart
@@ -4,111 +4,122 @@ import 'dart:convert' show json;
 import 'package:figma_squircle/figma_squircle.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:http/http.dart' as http;
+import 'package:mobile/services/authentication.dart';
+import 'package:mobile/screens/mainScreen.dart';
 
 
-class AuthenticationPage extends StatelessWidget{
+class AuthenticationPage extends StatefulWidget{
   AuthenticationPage({super.key});
 
   @override
+  State<AuthenticationPage> createState() => _AuthenticationPageState();
+}
+
+class _AuthenticationPageState extends State<AuthenticationPage>{
+
+  Authentication _authentication = Authentication();
+  GoogleSignInAccount? _currentAccount;
+
+
+  @override
   Widget build(BuildContext context) {
-    return Material(
-      type: MaterialType.transparency,
-      child : Column(
-        children: [
-          ClipPath(
-              clipper: SkewCut(),
-              child:Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height/2+100,
-                decoration: const BoxDecoration(
-                    gradient: LinearGradient(
-                        begin: Alignment.topRight,
-                        end: Alignment.bottomLeft,
-                        colors: <Color>[
-                          Color(0xFF894c56),
-                          Color(0xFFa80d0d)
-                        ]
+    if(_currentAccount == null) {
+      return Material(
+          type: MaterialType.transparency,
+          child: Column(
+            children: [
+              if(_currentAccount == null)
+                ClipPath(
+                    clipper: SkewCut(),
+                    child: Container(
+                      width: MediaQuery
+                          .of(context)
+                          .size
+                          .width,
+                      height: MediaQuery
+                          .of(context)
+                          .size
+                          .height / 2 + 100,
+                      decoration: const BoxDecoration(
+                          gradient: LinearGradient(
+                              begin: Alignment.topRight,
+                              end: Alignment.bottomLeft,
+                              colors: <Color>[
+                                Color(0xFF894c56),
+                                Color(0xFFa80d0d)
+                              ]
+                          )
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Image.asset("asset/logo.png", width: 200),
+                          const Text('Une toute petite étape avant de débuter',
+                              style: TextStyle(
+                                  fontSize: 35,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                              textAlign: TextAlign.center),
+
+                        ],
+                      ),
                     )
                 ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Image.asset("asset/logo.png",width: 200),
-                    const Text('Une toute petite étape avant de débuter',
-                        style: TextStyle(
-                            fontSize: 35,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold),
-                        textAlign: TextAlign.center),
-                    /*ElevatedButton(
-                      onPressed: _handleSignIn,
-                      child: const Text('SIGN IN'),
-                    ),*/
+              Container(
+                height: 100,
+              ),
+              Container(
+                width: 300,
+                decoration: ShapeDecoration(
+                  color: Colors.white,
+                  shape: SmoothRectangleBorder(
+                    borderRadius: SmoothBorderRadius(
+                      cornerRadius: 10,
+                      cornerSmoothing: 0.5,
+                    ),
+                  ),
+                  shadows: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: const Offset(0, 3), // changes position of shadow
+                    ),
                   ],
                 ),
+                child: MaterialButton(
+                  onPressed: ()  async {
+                    var _auth = await _authentication.signIn();
+                    setState(() {
+                       _currentAccount = _auth;
+                    });
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Image.asset("asset/google.png", width: 30),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      const Text("Continuer avec Google",
+                        style: TextStyle(color: Colors.grey, fontSize: 20),)
+                    ],
+                  ),),
               )
-          ),
-          Container(
-            height: 100,
-          ),
-          Container(
-            width: 300,
-            decoration: ShapeDecoration(
-              color: Colors.white,
-              shape: SmoothRectangleBorder(
-                borderRadius: SmoothBorderRadius(
-                  cornerRadius: 10,
-                  cornerSmoothing: 0.5,
-                ),
-              ),
-              shadows: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.5),
-                  spreadRadius: 5,
-                  blurRadius: 7,
-                  offset: const Offset(0, 3), // changes position of shadow
-                ),
-              ],
-            ),
-            child: MaterialButton(
-              onPressed: _handleSignIn,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Image.asset("asset/google.png",width: 30),
-                  const SizedBox(
-                    width: 20,
-                  ),
-                  const Text("Continuer avec Google",
-                    style: TextStyle(color: Colors.grey,fontSize: 20),)
-                ],
-              ),),
+
+            ],
           )
-
-        ],
-      )
-    );
-  }
-
-  final GoogleSignIn _googleSignIn = GoogleSignIn(
-    // Optional clientId
-    clientId:
-    '978419384807-l8725i106c9a3pl60st07i2gs39p1ct2.apps.googleusercontent.com',
-    scopes: <String>[
-      'email',
-      'openid',
-      'https://www.googleapis.com/auth/userinfo.profile',
-    ],
-  );
-
-  Future<void> _handleSignIn() async {
-    try {
-      await _googleSignIn.signIn();
-    } catch (error) {
-      print(error);
+      );
+    }
+    else{
+      return Material(
+        type: MaterialType.transparency,
+        child: MainScreen(_currentAccount)
+      );
     }
   }
+
+
 }
 
 class SkewCut extends CustomClipper<Path> {
