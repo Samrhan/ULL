d075cd06d31a62afcff482ea38ed1e48dc3b7e5a
EnzoFilangi <enzo.filangi@efrei.net>
2022-06-19 00:01:43 +0200

    feat(api) : Coded the portion of the script that uploads the provider profiles to the backend

---
+++ apps/dummy-data-scripts/src/data.ts
@@ -0,0 +1,98 @@
+import {
+  EditProviderInfoBody,
+  PriceUnit,
+  ProviderProfileSection,
+  RegisterProviderRequestBody,
+  SectionType
+} from "@ull/api-interfaces";
+
+export const providerData : {
+  register: RegisterProviderRequestBody,
+  profile_picture: string,
+  cover_picture: string
+  company_info: EditProviderInfoBody,
+  category: string,
+  tags: string[],
+  profile_content: ProviderProfileSection[]
+}[] = [
+  {
+    register: {
+      company_name: "Catering premium",
+      email: "Catering.premium@gmail.com",
+      password: "password",
+      phone: "+33601020304",
+      siren: "844639286"
+    },
+    profile_picture: '',
+    cover_picture: '',
+    company_info: {
+      company_name: "Catering premium",
+      company_description: "Un service traiteur de qualité",
+      area_served: "Région parisienne",
+      email: "Catering.premium@gmail.com",
+      phone: "+33601020304",
+      address: {
+        number: "1",
+        street: "Rue de la paix",
+        postal_code: "75010",
+        city: "Paris"
+      },
+    },
+    category: "",
+    tags: [""],
+    profile_content: [
+      {
+        id_section: "",
+        type: SectionType.big,
+        section_title: "Le bar",
+        section_description: "",
+        purchasable: true,
+        pictures: [
+          "images/waitress-gff8ebb643_1920.jpg",
+          "images/beer-820011_1920.jpg",
+        ],
+        content: [
+          {
+            id_performance: "",
+            performance_title: "Service au bar",
+            performance_description: "Nos cocktails, nos softs",
+            price: {
+              value: 1300,
+              unit: PriceUnit.person
+            },
+            picture: "images/cocktails.jpg",
+          }
+        ]
+      },
+      {
+        id_section: "",
+        type: SectionType.medium,
+        section_title: "Notre carte",
+        section_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam id vulputate dui, vel finibus nibh.",
+        purchasable: false,
+        content: [
+          {
+            id_performance: "",
+            performance_title: "Cupcakes",
+            performance_description: "De très bons cupcakes",
+            price: {
+              value: 500,
+              unit: PriceUnit.stack
+            },
+            picture: "images/cupcakes-gc0620a627_1920.jpg",
+          },
+          {
+            id_performance: "",
+            performance_title: "Tiramisu",
+            performance_description: "Meilleur dessert fait maison.\nLorem ipsum dolor sit amet, consectetur adipiscing elit. \nEtiam id vulputate dui, vel finibus nibh. Aliquam fringilla mi vel fermentum pulvinar. Ut venenatis nisi lorem, sit amet lacinia orci sollicitudin eget. In mattis est ex, eu tristique orci dignissim varius.",
+            price: {
+              value: 500,
+              unit: PriceUnit.stack
+            },
+            picture: "images/italian-food-2157246__340.png",
+          }
+        ]
+      }
+    ]
+  },
+]
--- apps/dummy-data-scripts/src/main.ts
+++ apps/dummy-data-scripts/src/main.ts
@@ -1,3 +1,229 @@
 import {environment} from "./environments/environment";
 import axios from "axios";
+import {providerData} from "./data";
+import * as readline from 'readline';
+import * as fs from 'fs';
+import {
+  EditProviderInfoBody, ProviderProfile,
+  ProviderProfileSection,
+  RegisterProviderRequestBody,
+  SectionType,
+  Performance
+} from "@ull/api-interfaces";
+import FormData from "form-data";
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function menu(){
+  rl.question(
+    "Please select the script you wish to run :\n" +
+    "\t1. Initialize provider (web) database with dummy data\n" +
+    "\t2. Initialize customer (mobile app) database with dummy data\n",
+    (result) => {
+      const choice = Number.parseInt(result, 10);
+      switch (choice){
+        case 1:
+          initProviderDatabase();
+          break;
+        case 2:
+          initCustomerDatabase().then();
+          break;
+        default:
+          console.log("Invalid input, please enter a valid option.");
+          menu();
+      }
+    }
+  );
+}
+menu()
+
+/**
+ * Uploads each provider profile from ./data.providerData
+ */
+function initProviderDatabase() : void {
+  // Handle each profile independently in parallel
+  providerData.forEach(profile => handleProvider(profile));
+}
+
+/**
+ * Uploads a provider to the backend.
+ *
+ * 1. Registers the provider
+ * 2. Logins to get the token
+ * 3. Sets the provider's company information
+ * 4. Adds all the sections to the provider's profile
+ * 5. Adds all the performances to the sections
+ * @param providerData
+ */
+async function handleProvider(providerData) : Promise<void> {
+  await registerProvider(providerData.register);
+  const userToken = await loginProvider(providerData.register);
+
+  setProviderInfos(userToken, providerData.company_info, providerData.profile_picture, providerData.cover_picture);
+  setProviderCategorization(userToken, providerData.category, providerData.tags);
+
+  setProviderServices(userToken, providerData.profile_content);
+}
+
+/**
+ * Register the provider with the given information
+ * @param body
+ */
+function registerProvider(body: RegisterProviderRequestBody) : Promise<void> {
+  return axios.post(environment.baseServerURL + environment.providerServiceURL + '/register', body);
+}
+
+/**
+ * Logins the user and returns a Promise that resolves with the access token
+ * @param body
+ */
+function loginProvider(body: RegisterProviderRequestBody) : Promise<string> {
+  return axios.post(environment.baseServerURL + environment.authenticationServiceURL + '/login', {
+    email: body.email,
+    password: body.password
+  });
+}
+
+/**
+ * Sets all the provided provider information, as well as the given pictures
+ *
+ * Fetches the pictures from disk with fs.readFileSync() to get a Buffer compatible with FormData
+ * @param userToken
+ * @param newInfo
+ * @param profilePicture
+ * @param coverPicture
+ */
+async function setProviderInfos(userToken: string, newInfo: EditProviderInfoBody, profilePicture: string, coverPicture: string) : Promise<void> {
+  // Not the real FormData from the browser, but a polyfill library for node
+  const formData = new FormData();
+  formData.append('company_name', newInfo.company_name);
+  formData.append('company_description', newInfo.company_description);
+  formData.append('email', newInfo.email);
+  formData.append('phone', newInfo.phone);
+  formData.append('area_served', newInfo.area_served);
+  formData.append('address_number', newInfo.address.number);
+  formData.append('address_street', newInfo.address.street);
+  formData.append('address_city', newInfo.address.city);
+  formData.append('address_postal_code', newInfo.address.postal_code);
+
+  if (newInfo.address.complement) {
+    formData.append('address_complement', newInfo.address.complement);
+  }
+
+  // This FormData accepts buffers for files instead of requiring a Blob which doesn't exist in node
+  formData.append('profile_picture', fs.readFileSync(profilePicture));
+  formData.append('cover_picture', fs.readFileSync(coverPicture));
+
+  // Specify that the content is of type form-data
+  const config = { headers: { 'Content-Type': 'multipart/form-data', 'Authorization' : `Bearer ${userToken}` } };
+
+  await axios.post(environment.baseServerURL + environment.providerServiceURL + '/profile', formData, config);
+}
+
+/**
+ * Sets the provider's category and tags to be the ones given as arguments
+ * @param userToken
+ * @param category
+ * @param tags
+ */
+async function setProviderCategorization(userToken: string, category: string, tags: string[]) : Promise<void> {
+  const config = { headers: { 'Authorization' : `Bearer ${userToken}` } };
+
+  await axios.put(environment.baseServerURL + environment.discoveryServiceURL + '/provider_category', {name: category}, config);
+  await axios.put(environment.baseServerURL + environment.discoveryServiceURL + '/provider_tags', tags, config);
+}
+
+/**
+ * Adds all the sections and performances to the user profile.
+ *
+ * 1. It adds all the sections in order, but doesn't fill them at first.
+ * 2. Then, it fetches the user's profile from the server to get the sections ids. This is needed as we do not know
+ *    what those ids will be before the backend creates the sections, but we need the ids to add the performances.
+ * 3. It goes through both lists in order, this way it has both the information about the content of the section
+ *    and its id. In the loop, it delegates to addPerformancesToSection() to do the actual work of adding all the
+ *    performances in order.
+ * @param userToken
+ * @param sections
+ */
+async function setProviderServices(userToken: string, sections: ProviderProfileSection[]) : Promise<void> {
+  // 1. Post the sections in the order they come in
+  for(const section of sections){
+    const config = { headers: { 'Content-Type': 'multipart/form-data', 'Authorization' : `Bearer ${userToken}` } };
+
+    const data = new FormData();
+    data.append('type', section.type);
+    data.append('section_title', section.section_title);
+    data.append('section_description', section.section_description || " ");
+    data.append('purchasable', section.purchasable.toString());
+
+    if (section.type === SectionType.small) {
+      data.append('preview_amount', JSON.stringify(section.preview_amount));
+    }
+
+    if (section.type === SectionType.big) {
+      for (const picture of section.pictures || []) {
+        // Append all the files to the same name to create an array
+        data.append('pictures', fs.readFileSync(picture));
+      }
+    }
+
+    // Very important await to make sure every addition is sequential
+    await axios.post(environment.baseServerURL + environment.providerServiceURL + '/section', data, config);
+  }
+
+  // 2. Fetch the user's profile
+  function getProviderId(token: string) : string {
+    const parsedToken = JSON.parse(atob(token.split('.')[1]))
+    return parsedToken.sub || parsedToken.id;
+  }
+  const config = { headers: { 'Authorization' : `Bearer ${userToken}` } };
+  const profile : ProviderProfile = await axios.get(environment.baseServerURL + environment.providerServiceURL + '/profile/' + getProviderId(userToken), config);
+  const profileSections : ProviderProfileSection[] = profile.services;
+
+  // 3. Run through both lists in order and add the relevant performances sequentially
+  for(let i = 0; i < profileSections.length; i++){
+    // The appending to sections is paralellized though as only the order of addition inside a section matter
+    // Therefore, no await here
+    addPerformancesToSection(userToken, profileSections[i].id_section, sections[i].content);
+  }
+}
+
+/**
+ * Adds the given list of performances to the section with the given id. It respects the order.
+ * @param userToken
+ * @param sectionId
+ * @param performances
+ */
+async function addPerformancesToSection(userToken: string, sectionId: string, performances: Performance[]) : Promise<void> {
+  const config = { headers: { 'Content-Type': 'multipart/form-data', 'Authorization' : `Bearer ${userToken}` } };
+
+  // Add every performance in order
+  for(const performance of performances){
+    const data = new FormData();
+    data.append('performance_title', performance.performance_title);
+    data.append('performance_description', performance.performance_description);
+    data.append('performance_picture', fs.readFileSync(performance.picture));
+    data.append('price_value', JSON.stringify(performance.price.value));
+    data.append('price_unit', performance.price.unit);
+    data.append('id_section', sectionId);
+
+    // Very important await to ensure sequentiality
+    await axios.post(environment.baseServerURL + environment.providerServiceURL + '/performance', data, config)
+  }
+}
+
+
+
+
+
+
+
+
+
+
+async function initCustomerDatabase() : Promise<void> {
+
+}
