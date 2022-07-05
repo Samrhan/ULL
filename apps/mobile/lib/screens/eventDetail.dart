c0468fc79c173ed2c35dcaccb1af2ecd5d54e195
shasless <Efrei2018>
2022-07-05 16:43:10 +0200

    fix( devrait pas pouvoir valider un projet vide)

--- apps/mobile/lib/screens/eventDetail.dart
+++ apps/mobile/lib/screens/eventDetail.dart
@@ -344,7 +344,7 @@ class _EventDetailState extends State<EventDetailStated>{
     final Environment ev = Environment();
     String url ="";    log(event.state);
     log(event.projectId.toString());
-    if(event.state == "draft"){
+    if(event.state == "draft" && nbPrestation > 0){
        url =
           ev.baseServer + ev.customerService + "/confirm/" + event.projectId.toString();
       }
