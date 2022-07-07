d1d75cdb0146b5e455fba56e62f57d043cd9ebfd
EnzoFilangi <enzo.filangi@efrei.net>
2022-07-07 07:54:45 +0200

    feat(dummy data) : added more profiles

--- apps/dummy-data-scripts/src/data.ts
+++ apps/dummy-data-scripts/src/data.ts
@@ -6,7 +6,7 @@ import {
   SectionType
 } from "@ull/api-interfaces";
 
-export const providerData : {
+export const providerData: {
   register: RegisterProviderRequestBody,
   profile_picture: string,
   cover_picture: string
@@ -382,29 +382,299 @@ export const providerData : {
   },
   {
     register: {
-      company_name: "",
-      email: "",
-      password: "",
-      phone: "+33",
-      siren: ""
+      company_name: "Les délices du Rajasthan",
+      email: "delice.rajasthan@outlook.com",
+      password: "passwd",
+      phone: "+33671212458",
+      siren: "564528652"
     },
-    profile_picture: '/images/',
-    cover_picture: '/images/',
+    profile_picture: '/images/Vegetarian_Curry.jpeg',
+    cover_picture: '/images/rajasthan_577.jpg',
     company_info: {
-      company_name: "",
-      company_description: "",
-      area_served: "",
-      email: "",
-      phone: "+33",
+      company_name: "Les délices du Rajasthan",
+      company_description: "Traiteur indien qui offre des plats de qualité pour vos papilles",
+      area_served: "Nord de Paris",
+      email: "delice.rajasthan@outlook.com",
+      phone: "+33671212458",
       address: {
-        number: "",
-        street: "",
-        postal_code: "",
-        city: ""
+        number: "23",
+        street: "rue de Bellevue",
+        postal_code: "75020",
+        city: "Paris"
       },
     },
-    category: "",
-    tags: [""],
+    category: "Traiteur",
+    tags: ["Cuisine Indienne", "Salle des fêtes"],
+    profile_content: []
+  },
+  {
+    register: {
+      company_name: "Nihongo Jouzu",
+      email: "nihongo.jouzu@gmail.com",
+      password: "dummypass",
+      phone: "+33650908070",
+      siren: "102608987"
+    },
+    profile_picture: '/images/japanese-food-940x625.jpg',
+    cover_picture: '/images/WallpaperStudio10japon-207132-1680x1050.jpg',
+    company_info: {
+      company_name: "Nihongo Jouzu",
+      company_description: "Découvrez les plats du Japon dans le restaurant Nihongo Jouzu",
+      area_served: "Ouest de Paris",
+      email: "nihongo.jouzu@gmail.com",
+      phone: "+33650908070",
+      address: {
+        number: "87",
+        street: "rue de Patrick Balkany",
+        postal_code: "92300",
+        city: "Levallois-Perret"
+      },
+    },
+    category: "Traiteur",
+    tags: ["Cuisine Japonaise", "Style Zen", "Livraison"],
+    profile_content: []
+  },
+  {
+    register: {
+      company_name: "El Pedro Alto",
+      email: "Elpedroalto@gmail.com",
+      password: "dummypass",
+      phone: "+33787986545",
+      siren: "434525987"
+    },
+    profile_picture: '/images/i52156-meilleures-recettes-mexicaines.jpg',
+    cover_picture: '/images/paysage-centre-mexique.jpg',
+    company_info: {
+      company_name: "El Pedro Alto",
+      company_description: "Venez dégustez les spécialités mexicaines",
+      area_served: "Nord de Paris",
+      email: "elpedroalto@gmail.com",
+      phone: "+33787986545",
+      address: {
+        number: "87",
+        street: "rue François Mitterand",
+        postal_code: "93000",
+        city: "Saint-Denis"
+      },
+    },
+    category: "Traiteur",
+    tags: ["Cuisine Mexicaine", "Livraison", "Fanfare"],
+    profile_content: []
+  },
+  {
+    register: {
+      company_name: "Kamsa Hamida",
+      email: "kamsa.resto@gmail.com",
+      password: "dummypass",
+      phone: "+33690654567",
+      siren: "852964758"
+    },
+    profile_picture: '/images/304090.jpg',
+    cover_picture: '/images/825071.jpg',
+    company_info: {
+      company_name: "Kamsa Hamida",
+      company_description: "Les délices de Corée du Sud",
+      area_served: "Est de Paris",
+      email: "kamsa.resto@gmail.com",
+      phone: "+33690654567",
+      address: {
+        number: "87",
+        street: "rue du château",
+        postal_code: "94080",
+        city: "Vincennes"
+      },
+    },
+    category: "Traiteur",
+    tags: ["Cuisine Coréenne", "Livraison"],
+    profile_content: []
+  },
+  {
+    register: {
+      company_name: "Rastapopoulos",
+      email: "rastapopoulos.resto@outlook.com",
+      password: "dummypass",
+      phone: "+33798781234",
+      siren: "108987456"
+    },
+    profile_picture: '/images/cuisine-grecque-flickr-9712150801_748a76b73e_h.jpg',
+    cover_picture: '/images/grce-santorin.jpg',
+    company_info: {
+      company_name: "Rastapopoulos",
+      company_description: "Découvrez les meilleurs plats grecs dans notre restaurant",
+      area_served: "Essonne",
+      email: "rastapopoulos.resto@outlook.com",
+      phone: "+33798781234",
+      address: {
+        number: "58",
+        street: "rue du prisme",
+        postal_code: "91100",
+        city: "Villabé"
+      },
+    },
+    category: "Traiteur",
+    tags: ["Cuisine Grecque", "Livraison", "Salle de fête"],
+    profile_content: []
+  },
+  {
+    register: {
+      company_name: "La fecha de hoy",
+      email: "lafechadehoy@outlook.com",
+      password: "dummypass",
+      phone: "+33787116685",
+      siren: "454856898"
+    },
+    profile_picture: '/images/1200x600-paella-1200x900.jpg',
+    cover_picture: '/images/8442_Fill_800_800.png',
+    company_info: {
+      company_name: "La fecha de hoy",
+      company_description: "Ce restaurant est vraiment le meilleur restaurant espagnol",
+      area_served: "Paris",
+      email: "lafechadehoy@outlook.com",
+      phone: "+33787116685",
+      address: {
+        number: "14",
+        street: "Avenue des Champs-Elysées",
+        postal_code: "75016",
+        city: "Paris"
+      },
+    },
+    category: "Traiteur",
+    tags: ["Cuisine Espagnole", "Livraison"],
+    profile_content: []
+  },
+  {
+    register: {
+      company_name: "Thaï hade",
+      email: "thaihade@gmail.com",
+      password: "dummypass",
+      phone: "+33698762076",
+      siren: "902685741"
+    },
+    profile_picture: '/images/On-fond-pour-le-pad-thai.jpg',
+    cover_picture: '/images/thailande.png',
+    company_info: {
+      company_name: "Thaï hade",
+      company_description: "Thaï hade est un restaurant thaï qui propose les plats les plus savoureux du monde",
+      area_served: "Nord de Paris",
+      email: "thaihade@gmail.com",
+      phone: "+33698762076",
+      address: {
+        number: "74",
+        street: "rue de la tranquilité",
+        postal_code: "Cergy",
+        city: "95000"
+      },
+    },
+    category: "Traiteur",
+    tags: ["Cuisine Thaï", "Livraison"],
+    profile_content: []
+  },
+  {
+    register: {
+      company_name: "La grande muraille",
+      email: "lagrandemuraille@outlook.com",
+      password: "dummypass",
+      phone: "+33618576622",
+      siren: "839461345"
+    },
+    profile_picture: '/images/plat-chinois-e1505832667470.jpg',
+    cover_picture: '/images/parc-wulingyuan-chine-wusuowei.jpeg',
+    company_info: {
+      company_name: "La grande muraille",
+      company_description: "La grande muraille est très accueillante et est un lieu de convivialité où vous pouvez manger de bonnes spécilalités",
+      area_served: "Paris",
+      email: "lagrandemuraille@outlook.com",
+      phone: "+33618576622",
+      address: {
+        number: "21",
+        street: "rue de la république",
+        postal_code: "75003",
+        city: "Paris"
+      },
+    },
+    category: "Traiteur",
+    tags: ["Cuisine Chinoise", "Salle de fête"],
+    profile_content: []
+  },
+  {
+    register: {
+      company_name: "L'oasis",
+      email: "oasis.resto@outlook.com",
+      password: "dummypass",
+      phone: "+33666328914",
+      siren: "971244663"
+    },
+    profile_picture: '/images/riz-au-poulet-et-legumes-343.640x480.jpg',
+    cover_picture: '/images/index.jpg',
+    company_info: {
+      company_name: "L'oasis",
+      company_description: "Venez déguster les délicieux plats du monde oriental",
+      area_served: "Sud de Paris",
+      email: "oasis.resto@outlook.com",
+      phone: "+33666328914",
+      address: {
+        number: "45",
+        street: "rue de la fraternité",
+        postal_code: "91300",
+        city: "Massy"
+      },
+    },
+    category: "Traiteur",
+    tags: ["Cuisine Orientale", "Livraison"],
+    profile_content: []
+  },
+  {
+    register: {
+      company_name: "Les saveurs de Kinshasa",
+      email: "saveurs.kinshasa@outlook.com",
+      password: "dummypass",
+      phone: "+33692728242",
+      siren: "105520312"
+    },
+    profile_picture: '/images/Les-repas-du-jeudi-a%CC%80-Gourgan-Congo.jpeg',
+    cover_picture: '/images/Congo.jpg',
+    company_info: {
+      company_name: "Les saveurs de Kinshasa",
+      company_description: "Découvrez les plats des différents pays de l'Afrique",
+      area_served: "Est de Paris",
+      email: "saveurs.kinshasa@outlook.com",
+      phone: "+33692728242",
+      address: {
+        number: "58",
+        street: "rue Paul Vaillant-Couturier",
+        postal_code: "77160",
+        city: "Provins"
+      },
+    },
+    category: "Traiteur",
+    tags: ["Cuisine Africaine", "Livraison"],
+    profile_content: []
+  },
+  {
+    register: {
+      company_name: "La cuisine des iles",
+      email: "cuisinedesiles@gmail.com",
+      password: "dummypass",
+      phone: "+33649264755",
+      siren: "452125954"
+    },
+    profile_picture: '/images/90a3d4af4b5f54456b7db973ac01c830-1920.jpg',
+    cover_picture: '/images/Paysage-Plage-Antilles.jpg',
+    company_info: {
+      company_name: "La cuisine des iles",
+      company_description: "La cuisine des Iles vous sertses meilleurs plats",
+      area_served: "Paris",
+      email: "cuisinedesiles@gmail.com",
+      phone: "+33649264755",
+      address: {
+        number: "22",
+        street: "rue Stalingrad",
+        postal_code: "75019",
+        city: "Paris"
+      },
+    },
+    category: "Traiteur",
+    tags: ["Cuisine Antillaise"],
     profile_content: []
   }
 ]
